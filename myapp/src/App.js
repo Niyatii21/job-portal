84788444c075ad26104818bf392b45a250e7bec9
Parth Rane <parthrane7227@gmail.com>
2024-07-01 17:31:14 +0530

    Worked on Footer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './Footer/Footer';
 import { Menu } from './Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Menu/>
+      <Footer/>
     </div>
   );
 }
