ff58e94e89a9e6560668ea46b32f4dac5f861932
Muhammed Bilal <muhammedbilal6211@gmail.com>
2024-07-02 16:26:34 +0530

    create user and otp schema

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import 'dotenv/config'
-
-
+import dotenv from 'dotenv'
+dotenv.config();
+import cookieParser from "cookie-parser";
+import connectDb from './Config/db.js'
+import cors from 'cors'
+import path from 'path';
+const PORT = process.env.PORT || 5000
 const app = express()
 
+connectDb()
 
-const PORT = process.env.PORT || 5000
 
 
 
